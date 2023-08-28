a8046d7d95c846df2986bf403e45ede75f54b57b
Pedro Henrique Vila Nova <Pedroh.vnsantos@hotmail.com>
2023-08-28 17:40:15 -0300

    commit mudancas variaveis e operadores

---
+++ Primeiras Páginas Interativas com Javascript/Sintáxe Básica com Javascript/Aula 02 - Variaveis/script.js
@@ -0,0 +1,116 @@
+//Tupos Primitivos
+
+// Boolean
+
+var VouF = false ; 
+console.log(typeof(VouF)) ;
+
+//number
+
+var numeroQualquer = 5;
+console.log(typeof(numeroQualquer) );
+
+//string
+
+var nome = "Nome";
+console.log(typeof(nome));
+
+//Declaração de variáveis
+
+//var
+
+var variavel ;
+variavel = "Diana" ;
+console.log(variavel) ;
+
+//let
+
+let variavel2 = "Regina" ;
+variavel2 = "Martine" ; 
+console.log(variavel2) ; 
+
+//const
+
+const constante = "Ana" ;
+console.log(constante);
+
+//Escopo Global e local
+
+var escopoGlobal = "global" ;
+console.log(escopoGlobal);
+
+function escopoLocal(){
+    let escopoLocalInterno = "local" ;
+    console.log(escopoLocalInterno) ; 
+}
+
+escopoLocal();
+
+//Atribuição, Comparação e Comparação Idêntica
+
+//Atribuição
+
+var atribuicao = 0;
+console.log(atribuicao);
+
+//Comparação
+
+var comparacao = "0" == 0 ;
+console.log(comparacao);
+
+//Comparação Idêntica
+
+var comparacaoIdentica = "0" === 0 ; 
+console.log(comparacaoIdentica);
+
+//Operadores aritméticos, Relacionais e Lógicos
+
+//Operadores Aritméticos - adicao
+
+console.log(1 + 1);
+
+//Operadores Aritméticos - subtracao
+
+console.log(2 - 1);
+
+//Operadores Aritméticos - multiplicacao
+
+console.log(1 * 1);
+
+//Operadores Aritméticos - divisao real
+
+console.log(2 / 2);
+
+//Operadores Aritméticos - divisao inteira
+
+console.log(5 % 2);
+
+//Operadores Aritméticos - potenciacao
+
+console.log(1 ** 1);
+
+//Operadores Relacionais - Maior que
+
+console.log(5 > 2);
+
+//Operadores Relacionais - Menor que
+
+console.log(5 < 2);
+
+//Operadores Relacionais - Maior igual a 
+
+console.log(5 >= 2);
+
+//Operadores Relacionais - Menor igual a 
+
+console.log(5 <= 2);
+
+//Operadores Lógicos - E
+
+
+
+//Operadores Lógicos - Ou
+
+
+
+//Operadores Lógicos - Não
