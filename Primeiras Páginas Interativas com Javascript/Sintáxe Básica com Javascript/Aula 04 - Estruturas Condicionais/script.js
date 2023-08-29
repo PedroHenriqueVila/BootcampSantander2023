f9ec804e7bf2d68372c3a583a18736e0e1dc5ddd
Pedro Henrique Vila Nova <Pedroh.vnsantos@hotmail.com>
2023-08-29 17:19:36 -0300

    commit estruturas condicionais e de repeticao

--- Primeiras Páginas Interativas com Javascript/Sintáxe Básica com Javascript/Aula 04 - Estruturas Condicionais/script.js
+++ Primeiras Páginas Interativas com Javascript/Sintáxe Básica com Javascript/Aula 04 - Estruturas Condicionais/script.js
@@ -20,6 +20,8 @@ if (jogador1 > 0 && jogador2 == 0) {
      console.log('Ninguém marcou ponto');
 }
 
+//Switch sendo usado como condicional
+
 switch (placar) {
     case placar = jogador1 > jogador2 : 
         console.log('Jogador 1 ganhou');
@@ -32,3 +34,50 @@ switch (placar) {
 }
 
 
+// Estruturas de Repetição
+
+// for
+
+let array = ['valor1','valor2','valor3','valor4','valor5'];
+let object = {propriedade1: 'valor1' , propriedade2: 'valor2' , propriedade3: 'valor3' };
+
+for (let indice = 0; indice < array.length; indice ++){
+    console.log(indice);
+}
+
+// for/in
+
+for (let i in array){
+    console.log(i);
+}
+
+for (i in object){
+    console.log(i);
+}
+
+
+// for/of
+
+for (i of array){
+    console.log(i);
+}
+
+for (i of object.propriedade1){
+    console.log(i);
+}
+
+// while
+
+var a = 0
+while (a < 10){
+    a++ 
+    console.log(a);
+}
+
+// Do/while
+
+var b = 0;
+do{
+    b++ ;
+    console.log(b);
+} while (b < 10)
