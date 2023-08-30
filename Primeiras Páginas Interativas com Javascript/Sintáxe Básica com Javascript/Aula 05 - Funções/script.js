fd6bd5dbc0f924b33e8cd0aa6d097078ef5dc000
Pedro Henrique Vila Nova <Pedroh.vnsantos@hotmail.com>
2023-08-29 22:08:41 -0300

    commit funções e calculadora

---
+++ Primeiras Páginas Interativas com Javascript/Sintáxe Básica com Javascript/Aula 05 - Funções/script.js
@@ -0,0 +1,16 @@
+//Funções
+
+function funcao () {
+    console.log('Tudo certo jovem!') ;
+
+}
+
+funcao ();
+
+// c/ Parâmetro
+
+function mensagem (primeiro, segundo){
+    console.log(primeiro, segundo);
+}
+
+mensagem ('tudo certo jobvem');
---
+++ Primeiras Páginas Interativas com Javascript/Sintáxe Básica com Javascript/Aula 06 - Aprofundando em funções/tiposDeFuncao.js
@@ -0,0 +1,31 @@
+// Tipos de funcao
+
+// Declarativas
+
+function funcao (){
+    console.log('Sou uma mensagem declarativa');
+}
+
+funcao ();
+
+// Expressões de função
+
+var funcao1 = function funcao2(){
+    console.log('sou uma mensagem expressiva');
+}
+
+funcao1 ();
+
+var funcao3 = function(){
+    console.log('sou uma mensagem expressiva');
+}
+
+funcao3 ();
+
+// Arrow Function
+
+var funcaoArrow = () => {
+    console.log('Sou uma arrow function');
+}
+
+funcaoArrow ();
