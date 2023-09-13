9aa56f077d0ed55efd5a6f7711e52d66acb73dfa
Pedro Henrique Vila Nova <Pedroh.vnsantos@hotmail.com>
2023-09-13 17:24:32 -0300

    commit Finalizacao do Curso - Inclusao de limitador de Pokemons

--- Introdução ao Desenvolvimento Web com JavaScript/assets/js/main.js
+++ Introdução ao Desenvolvimento Web com JavaScript/assets/js/main.js
@@ -1,7 +1,11 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
-const limit = 5
+
+const maxRecords = 151
+const limit = 10
 let offset = 0;
+
+
 
 
 function loadPokemonItens(offset, limit) {
@@ -33,7 +37,16 @@ loadPokemonItens(offset, limit)
 
 loadMoreButton.addEventListener('click', () => {
     offset += limit
-    loadPokemonItens(offset, limit)
+    const qtdRecordsWithNextPage = offset + limit
+
+    if (qtdRecordsWithNextPage >= maxRecords) {
+        const newLimit = maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+    } else {
+        loadPokemonItens(offset, limit)
+    }
 })
 
 
