8ecf84dfef747bc44ca94e0fb0ef0e674f904e41
Pedro Henrique Vila Nova <Pedroh.vnsantos@hotmail.com>
2023-09-19 17:58:11 -0300

    commit Contruindo um Component de maneita profissional

--- Ganhando Produtividade com Angular/Trabalhando com Web Components no Front-end/src/Components/cardNews.js
+++ Ganhando Produtividade com Angular/Trabalhando com Web Components no Front-end/src/Components/cardNews.js
@@ -1,11 +1,54 @@
-
 class cardNews extends HTMLElement {
     constructor() {
         super();
 
-        const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1>Hello World</h1>";
+        const shadow = this.attachShadow({mode: "open"})
+        shadow.appendChild(this.build())
+        shadow.appendChild(this.styles())
+    }
+
+    build() {
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class" , "card");
+
+
+            const cardLeft = document.createElement("div");
+            cardLeft.setAttribute("class" , "card_left");
+
+                const autor = document.createElement("span");
+                autor.textContent = "By " + (this.getAttribute("autor") || "Anonymous");
+
+                const linkTitle = document.createElement("a");
+                linkTitle.textContent = this.getAttribute("title");
+                linkTitle.href = this.getAttribute("link-url");
+
+                const newsContent = document.createElement("p");
+                newsContent.textContent = this.getAttribute("content");
+
+                cardLeft.appendChild(autor);
+                cardLeft.appendChild(linkTitle);
+                cardLeft.appendChild(newsContent);
+
+
+            const cardRigth = document.createElement("div");
+            cardRigth.setAttribute("class" , "card_rigth");
+
+                const newsImage = document.createElement("img");
+                newsImage.src = this.getAttribute("photo") || "assets/foto-default.jpg";
+                newsImage.alt = "Foto da Noticia";
+
+                cardRigth.appendChild(newsImage);
+
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRigth);
+
+        return componentRoot;
+    }
+ 
+    styles() {
+
     }
 }
 
-customElements.define('card-news' , cardNews)
+customElements.define("card-news" , cardNews);
